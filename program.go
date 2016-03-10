5aa7775908bf4373815e53bd9c2a006f8576f8b5
rogeralsing <rogeralsing@gmail.com>
2016-03-10 22:28:06 +0100

    working

--- program.go
+++ program.go
@@ -2,20 +2,52 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/rhinoman/couchdb-go"
 	"github.com/rogeralsing/hellogo/util"
 	"net/http"
 	"os"
+	"time"
 )
+
+type PersonDocument struct {
+	Name     string   `json:"name"`
+	Age      int      `json:"age"`
+	Children []string `json:"children"`
+}
 
 func main() {
 	//gin.SetMode(gin.ReleaseMode)
 	router := gin.Default()
 	util.Hello()
+	var timeout = time.Duration(500 * time.Millisecond)
+	conn, _ := couchdb.NewConnection("127.0.0.1", 5984, timeout)
+	//auth := couchdb.BasicAuth{Username: "user", Password: "password" }
+	db := conn.SelectDB("mydb", nil)
+
+	router.GET("/bar/:id", func(c *gin.Context) {
+		id := c.Param("id")
+		var res PersonDocument
+		db.Read(id, &res, nil)
+		c.JSON(200, res)
+	})
 
 	router.GET("/foo", func(c *gin.Context) {
 
+		theDoc := PersonDocument{
+			Name:     "Olle",
+			Age:      80,
+			Children: []string{"Arne", "Pelle", "Eva"},
+		}
+
+		theId := "mittid123"
+		//The third argument here would be a revision, if you were updating an existing document
+		_, _ = db.Save(theDoc, theId, "")
+
+		var x PersonDocument
+		db.Read(theId, &x, nil)
+
 		res, _ := os.Hostname()
-		c.String(http.StatusOK, "Hello %s", res)
+		c.String(http.StatusOK, "Hello %s %s", res, x.Children[0])
 	})
 
 	// This handler will match /user/john but will not match neither /user/ or /user
