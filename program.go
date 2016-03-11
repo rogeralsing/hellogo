ab4deac1d802ca56162323a57bf551efdedf1a93
rogeralsing <rogeralsing@gmail.com>
2016-03-11 08:58:05 +0100

    working

--- program.go
+++ program.go
@@ -4,15 +4,14 @@ import (
 	"github.com/gin-gonic/gin"
 	"github.com/rhinoman/couchdb-go"
 	"github.com/rogeralsing/hellogo/util"
-	"net/http"
-	"os"
 	"time"
+	"net/http"
 )
 
 type PersonDocument struct {
-	Name     string   `json:"name"`
-	Age      int      `json:"age"`
-	Children []string `json:"children"`
+	Name     string   `form:"name" json:"name"`
+	Age      int      `form:"age" json:"age"`
+	Children []string `"form:children" json:"children"`
 }
 
 func main() {
@@ -26,43 +25,22 @@ func main() {
 
 	router.GET("/bar/:id", func(c *gin.Context) {
 		id := c.Param("id")
-		var res PersonDocument
-		db.Read(id, &res, nil)
-		c.JSON(200, res)
+		var doc PersonDocument
+		db.Read(id, &doc, nil)
+		c.JSON(http.StatusOK, doc)
 	})
 
-	router.GET("/foo", func(c *gin.Context) {
+	router.PUT("/bar/:id", func(c *gin.Context) {
+		id := c.Param("id")
+		var doc PersonDocument
+		rev, _ := db.Read(id, &doc, nil)
 
-		theDoc := PersonDocument{
-			Name:     "Olle",
-			Age:      80,
-			Children: []string{"Arne", "Pelle", "Eva"},
+		if err := c.BindJSON(&doc);err == nil {
+			db.Save(doc, id, rev)
+			c.JSON(http.StatusOK, doc)
+		} else {
+			c.JSON(http.StatusBadRequest, err.Error())
 		}
-
-		theId := "mittid123"
-		//The third argument here would be a revision, if you were updating an existing document
-		_, _ = db.Save(theDoc, theId, "")
-
-		var x PersonDocument
-		db.Read(theId, &x, nil)
-
-		res, _ := os.Hostname()
-		c.String(http.StatusOK, "Hello %s %s", res, x.Children[0])
-	})
-
-	// This handler will match /user/john but will not match neither /user/ or /user
-	router.GET("/user/:name", func(c *gin.Context) {
-		name := c.Param("name")
-		c.String(http.StatusOK, "Hello %s", name)
-	})
-
-	// However, this one will match /user/john/ and also /user/john/send
-	// If no other routers match /user/john, it will redirect to /user/john/
-	router.GET("/user/:name/*action", func(c *gin.Context) {
-		name := c.Param("name")
-		action := c.Param("action")
-		message := name + " is " + action
-		c.String(http.StatusOK, message)
 	})
 
 	router.Run("0.0.0.0:8080")
