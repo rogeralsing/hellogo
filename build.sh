d637296186ddb96c53eaa6d4999febe921bda600
rogeralsing <rogeralsing@gmail.com>
2016-03-12 16:43:07 +0100

    working

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 echo compiling Go via Docker
-docker run -it --rm -v /home/rogeralsing/go:/app -e GOPATH="/app" -w "/app/src/github.com/rogeralsing/hellogo" golang sh -c 'CGO_ENABLED=0 go build -a --installsuffix cgo --ldflags="-s" -o hello'
+docker run -it --rm -v ${GOPATH}:/app -e GOPATH="/app" -w "/app/src/github.com/rogeralsing/hellogo" golang sh -c 'CGO_ENABLED=0 go build -a --installsuffix cgo --ldflags="-s" -o hello'
 echo Building Docker image
 docker build -t rogeralsing/hellogo .
 docker push rogeralsing/hellogo
--- program.go
+++ program.go
@@ -13,45 +13,47 @@ import (
 type PersonDocument struct {
 	Name     string   `form:"name" json:"name"`
 	Age      int      `form:"age" json:"age"`
-	Children []string `"form:children" json:"children"`
+	Children []string `form:"children" json:"children"`
 }
 
 func main() {
 	//gin.SetMode(gin.ReleaseMode)
 	router := gin.Default()
-	util.Hello()
 	timeout := time.Duration(500 * time.Millisecond)
 	port, _ := strconv.Atoi(os.Getenv("PORT"))
 	host := os.Getenv("HOST")
 	println("CouchDB Address ", host, port)
-
-	if conn, err := couchdb.NewConnection(host, port, timeout); err == nil {
-		db := conn.SelectDB("mydb", nil)
-
-		router.GET("/bar/:id", func(c *gin.Context) {
-			id := c.Param("id")
-			var doc PersonDocument
-			db.Read(id, &doc, nil)
-			c.JSON(http.StatusOK, doc)
-		})
-
-		router.PUT("/bar/:id", func(c *gin.Context) {
-			id := c.Param("id")
-			var doc PersonDocument
-			rev, _ := db.Read(id, &doc, nil)
-
-			if err := c.BindJSON(&doc); err == nil {
-				db.Save(doc, id, rev)
-				c.JSON(http.StatusOK, doc)
-			} else {
-				c.JSON(http.StatusBadRequest, err.Error())
-			}
-		})
-
-		router.Run("0.0.0.0:8080")
-	} else {
+	conn, err := couchdb.NewConnection(host, port, timeout);
+	if err != nil {
 		panic(err.Error())
 	}
-	//auth := couchdb.BasicAuth{Username: "user", Password: "password" }
+	conn.CreateDB("mydb", nil)
+	db := conn.SelectDB("mydb", nil)
 
+	router.GET("/bar/:id", func(c *gin.Context) {
+		id := c.Param("id")
+		var doc PersonDocument
+		if _, err := db.Read(id, &doc, nil); err != nil {
+			c.JSON(http.StatusInternalServerError, err.Error())
+			return
+		}
+		c.JSON(http.StatusOK, doc)
+	})
+
+	router.PUT("/bar/:id", func(c *gin.Context) {
+		id := c.Param("id")
+		var doc PersonDocument
+		rev, _ := db.Read(id, &doc, nil)
+
+		if err := c.BindJSON(&doc); err != nil {
+			c.JSON(http.StatusBadRequest, err.Error())
+			return
+		}
+		if _, err := db.Save(doc, id, rev); err != nil {
+			c.JSON(http.StatusInternalServerError, err.Error())
+		}
+		c.JSON(http.StatusOK, doc)
+	})
+
+	router.Run("0.0.0.0:8080")
 }
