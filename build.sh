fd5b3f6839d3920f415c621bdbb23e77e95862ba
rogeralsing <rogeralsing@gmail.com>
2016-03-13 22:18:02 +0100

    working

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-echo compiling Go via Docker
-docker run -it --rm -v ${GOPATH}:/app -e GOPATH="/app" -w "/app/src/github.com/rogeralsing/hellogo" golang sh -c 'CGO_ENABLED=0 go build -a --installsuffix cgo --ldflags="-s" -o hello'
-echo Building Docker image
-docker build -t rogeralsing/hellogo .
-docker push rogeralsing/hellogo
-
-#set HOST to "DB", PORT to 5984, link "DB" to CouchDB container
-#docker run -p 8080:8080 -e HOST=DB -e PORT=5984 --link 991dfe34173f:DB rogeralsing/hellogo
