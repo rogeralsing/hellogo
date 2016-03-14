a2c57fb1f5aa86c401a36cc4a59e0cae7a091c75
rogeralsing <rogeralsing@gmail.com>
2016-03-14 19:46:29 +0100

    working

--- person/person_test.go
+++ person/person_test.go
@@ -1,14 +1,14 @@
 package person
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/stretchr/testify/assert"
 	"net/http"
 	"net/http/httptest"
 	"testing"
-	"bytes"
-	"encoding/json"
 )
 
 type TestPersonRepository struct {
@@ -28,39 +28,55 @@ func (this TestPersonRepository) SavePerson(id string, person Person) error {
 	return nil
 }
 
+func GetPerson() Person{
+	return Person{
+		Name: "Roger",
+		Age: 40,
+		Children: []string{"Alex", "Alva", "Alice", "Theo"},
+	}
+}
+
 func TestGetPerson(t *testing.T) {
+	//arrange
 	gin.SetMode(gin.TestMode)
 	router := gin.Default()
-	testPersonDB := TestPersonRepository{people: map[string]Person{"roger": Person{Name: "Roger", Age: 40}}}
+	inputPerson := GetPerson()
+	testPersonDB := TestPersonRepository{people: map[string]Person{"roger": inputPerson}}
 	CreatePersonService(router, testPersonDB)
+
+	//act
 	req, _ := http.NewRequest("GET", "/api/v1/person/roger", nil)
 	resp := httptest.NewRecorder()
 	router.ServeHTTP(resp, req)
+
+	//assert
 	var person Person
-	json.Unmarshal(resp.Body.Bytes(),&person)
-	assert.Equal(t, "Roger", person.Name )
-	assert.Equal(t, 40, person.Age )
+	json.Unmarshal(resp.Body.Bytes(), &person)
+	assert.Equal(t, inputPerson.Name, person.Name)
+	assert.Equal(t, inputPerson.Age, person.Age)
+	assert.Equal(t, inputPerson.Children, person.Children)
 }
 
 func TestPutPerson(t *testing.T) {
+	//arrange
 	gin.SetMode(gin.TestMode)
 	router := gin.Default()
 	testPersonDB := TestPersonRepository{people: map[string]Person{}}
 	CreatePersonService(router, testPersonDB)
+	inputPerson := GetPerson()
+	b, _ := json.Marshal(inputPerson)
 
-	inputPerson := Person{Name:"Roger",Age:40,Children:[]string{"Alex","Alva","Alice","Theo"}}
-	b,_ :=json.Marshal(inputPerson)
-
+	//act
 	req, _ := http.NewRequest("PUT", "/api/v1/person/roger", bytes.NewBuffer(b))
 	req.Header.Set("X-Custom-Header", "myvalue")
 	req.Header.Set("Content-Type", "application/json")
-
 	resp := httptest.NewRecorder()
 	router.ServeHTTP(resp, req)
 
-	person,ok := testPersonDB.people["roger"]
-	assert.True(t,ok,"Person not found")
-	assert.Equal(t,inputPerson.Name,person.Name)
-	assert.Equal(t,inputPerson.Age,person.Age)
-	assert.Equal(t,inputPerson.Children,person.Children)
+	//assert
+	person, ok := testPersonDB.people["roger"]
+	assert.True(t, ok, "Person not found")
+	assert.Equal(t, inputPerson.Name, person.Name)
+	assert.Equal(t, inputPerson.Age, person.Age)
+	assert.Equal(t, inputPerson.Children, person.Children)
 }
